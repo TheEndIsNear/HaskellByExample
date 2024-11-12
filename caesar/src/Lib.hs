6a17abac8570d86c2ef5f27f954b0bcb847cd825
TheEndIsNear <2172212+TheEndIsNear@users.noreply.github.com>
2024-11-11 18:30:49 -0700

    add the rot135 function

--- caesar/src/Lib.hs
+++ caesar/src/Lib.hs
@@ -1,4 +1,4 @@
-module Lib (caesar, rot13) where
+module Lib (caesar, rot13, rot135) where
 
 type Alphabet = [Char]
 
@@ -28,7 +28,7 @@ indexOf _ch [] = undefined
 indexOf ch (x : xs) = if x == ch then 0 else 1 + indexOf ch xs
 
 alphabetRot :: Alphabet -> Int -> Char -> Char
-alphabetRot alphabet n ch = alphabet !! ((indexOf ch alphabet + n) `mod` 26)
+alphabetRot alphabet n ch = alphabet !! ((indexOf ch alphabet + n) `mod` length alphabet)
 
 upperRot :: Int -> Char -> Char
 upperRot = alphabetRot upperAlphabet
@@ -36,10 +36,15 @@ upperRot = alphabetRot upperAlphabet
 lowerRot :: Int -> Char -> Char
 lowerRot = alphabetRot lowerAlphabet
 
+digitRot :: Int -> Char -> Char
+digitRot = alphabetRot digits
+
 rotChar :: Int -> Char -> Char
 rotChar n ch
   | isLower ch = lowerRot n ch
   | isUpper ch = upperRot n ch
+  | isDigit ch = digitRot n ch
+  | isMisc ch = ch
   | otherwise = ch
 
 caesar :: Int -> String -> String
@@ -47,3 +52,12 @@ caesar n = map (rotChar n)
 
 rot13 :: String -> String
 rot13 = caesar 13
+
+rot135 :: String -> String
+rot135 = map rotSingleChar
+  where
+    rotSingleChar ch
+      | isDigit ch = rotChar 5 ch
+      | isLower ch || isUpper ch = rotChar 13 ch
+      | isMisc ch = ch
+      | otherwise = ch
