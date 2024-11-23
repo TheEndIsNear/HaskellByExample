8eb98aa84b6f63e17f4fcd4b5ce4cd94b7278cba
TheEndIsNear <2172212+TheEndIsNear@users.noreply.github.com>
2024-11-22 17:45:18 -0700

    work on counting number of lines in a file

--- lnums/src/Lib.hs
+++ lnums/src/Lib.hs
@@ -1,8 +1,75 @@
 module Lib
-    ( parseArguments 
-    ) where
+  ( parseArguments,
+    readLines,
+    numberAllLines,
+    numberNonEmptyLines,
+    numberAndIncrementNonEmptyLines,
+    padRight,
+    padLeft,
+    padCenter,
+  )
+where
+
+import Data.Char
+
+type NumberedLine = (Maybe Int, String)
+
+type NumberedLines = [NumberedLine]
+
+data PadMode = PadLeft | PadRight | PadCenter
 
 parseArguments :: [String] -> Maybe FilePath
 parseArguments [filepath] = Just filepath
 parseArguments _ = Nothing
 
+readLines :: FilePath -> IO [String]
+readLines filePath = do
+  contents <- readFile filePath
+  return (lines contents)
+
+isEmpty :: String -> Bool
+isEmpty str =
+  null str
+    || all (\s -> not (isPrint s) || isSeparator s) str
+
+isNotEmpty :: String -> Bool
+isNotEmpty = not . isEmpty
+
+numberLines :: (String -> Bool) -> (String -> Bool) -> [String] -> NumberedLines
+numberLines shouldIncr shouldNumber text =
+  let go :: Int -> [String] -> NumberedLines
+      go _ [] = []
+      go counter (x : xs) =
+        let mNumbering = if shouldNumber x then Just counter else Nothing
+            newCounter = if shouldIncr x then counter + 1 else counter
+         in (mNumbering, x) : go newCounter xs
+   in go 1 text
+
+numberAllLines :: [String] -> NumberedLines
+numberAllLines = numberLines (const True) (const True)
+
+numberNonEmptyLines :: [String] -> NumberedLines
+numberNonEmptyLines = numberLines (const True) isNotEmpty
+
+numberAndIncrementNonEmptyLines :: [String] -> NumberedLines
+numberAndIncrementNonEmptyLines = numberLines isNotEmpty isNotEmpty
+
+pad :: PadMode -> Int -> String -> String
+pad mode n str =
+  let diff = n - length str
+      centerDiff = div (n - length str) 2
+      padding = replicate diff ' '
+      centerPadding = replicate centerDiff ' '
+   in case mode of
+        PadLeft -> padding ++ str
+        PadRight -> str ++ padding
+        PadCenter -> centerPadding ++ str ++ centerPadding
+
+padLeft :: Int -> String -> String
+padLeft = pad PadLeft
+
+padRight :: Int -> String -> String
+padRight = pad PadRight
+
+padCenter :: Int -> String -> String
+padCenter = pad PadCenter
